df067ce16fbddc2128d5bbc60be8b47f17bb40a7
ifti <31423256+i-f-t-i@users.noreply.github.com>
2017-10-01 14:07:06 -0400

    Created vendor invoice under language

---
+++ upload/admin/language/en-gb/billing/vendor_invoice.php
@@ -0,0 +1,80 @@
+<?php
+// Heading
+$_['heading_title']				= 'Vendor Invoices';
+
+// Column
+$_['column_invoice_id']			= 'Invoice ID';
+$_['column_name']				= 'Name';
+$_['column_total']				= 'Total';
+$_['column_date_issued']		= 'Date Issued';
+$_['column_date_due']			= 'Date Due';
+$_['column_date_modified']		= 'Last Modified';
+$_['column_journal_entry']		= 'Journal Entry';
+$_['column_action']				= 'Action';
+$_['column_number']				= 'No.';
+$_['column_title']				= 'Title';
+$_['column_description']		= 'Description';
+$_['column_quantity']		    = 'Quantity';
+$_['column_price']				= 'Price';
+$_['column_tax_class']			= 'Tax Class';
+$_['column_discount']			= 'Discount';
+$_['column_total']				= 'Total';
+$_['column_status']				= 'Status';
+$_['column_comment']			= 'Comment';
+$_['column_date_added']			= 'Date Added';
+
+
+// Entry
+
+
+
+$_['entry_invoice_id']			= 'Invoice ID';
+$_['entry_vendor']			    = 'Vendor';
+$_['entry_name']			    = 'Vendor Name';
+$_['entry_email']				= 'Email Address';
+$_['entry_phone']			    = 'Phone';
+$_['entry_address']			    = 'Address';
+$_['entry_invoiceno']			= 'Invoice no';
+$_['entry_date_due']			= 'Date Due';
+$_['entry_payment_address']	= 'Payment Address';
+$_['entry_payment_email']	= 'Payment Email';
+$_['entry_payment_phone']	= 'Payment Phone';
+$_['entry_payment_code']	= 'Paid Invoice by';
+$_['entry_payment_description']	= 'Payment Description';
+$_['entry_currency_code']	= 'Currency Code';
+$_['entry_currency_value']	= 'Currency Value';
+
+
+
+
+// Tab
+$_['tab_general']				= '1. General';
+$_['tab_payment_detail']		= '2. Payment Details';
+$_['tab_payment_method']		= '3. Payment Method';
+$_['tab_item']					= '4. Items';
+$_['tab_total']					= '5. Review';
+
+
+// Text
+$_['text_no_results']			= 'There is no invoice to list.';
+$_['text_success']				= 'You have successfully modified invoices.';
+$_['text_no_histories']			= 'There is no history to list.';
+$_['text_updated']				= 'Updated:';
+$_['text_issued']				= 'Issued:';
+$_['text_item']					= 'Items';
+$_['text_payment']				= 'Payment';
+$_['text_inventory_id']			= 'Inventory ID';
+
+// Tooltip
+$_['tooltip_price']             = 'Price per quantity, excluding taxes.';
+
+// Error
+$_['error_permission']			= 'You do not have permission to modify invoices.';
+$_['error_email']	 			= 'Email address does not seem to be valid.';
+$_['error_payment_name']	 	= 'Payment name must not be empty.';
+$_['error_currency_code']	 	= 'Currency must be selected.';
+$_['error_currency_value']	 	= 'Currency value must be greater than 0.';
+$_['error_date_due']	 		= 'Date due must not be empty.';
+$_['error_item']	 			= 'At least one item is required for total calculation.';
+$_['error_payment-description']	= 'Payment description must not be empty.';
+
